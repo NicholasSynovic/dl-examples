e64122bcf12a01c334391b6171c6406c15885636
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-27 15:37:16 -0500

    Move common variables to a file

--- dl_examples/cv/alexnet/alexnet_keras.py
+++ dl_examples/cv/alexnet/alexnet_keras.py
@@ -10,9 +10,13 @@ Model Implementation Author: Nicholas M. Synovic
 from keras import Sequential
 from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPool2D, ReLU
 
+from dl_examples.cv.alexnet.common import Common
+
 
 class AlexNet:
-    def __init__(self, outputUnits: int = 1000) -> None:
+    def __init__(self) -> None:
+        self.common: Common = Common()
+
         self.model: Sequential = Sequential(name="AlexNet")
 
         self.model.add(layer=Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4)))
@@ -23,27 +27,47 @@ class AlexNet:
         self.model.add(layer=ReLU())
         self.model.add(layer=MaxPool2D())
 
-        self.model.add(layer=Conv2D(filters=384, kernel_size=(3, 3)))
+        self.model.add(
+            layer=Conv2D(
+                filters=384,
+                kernel_size=self.common.kernelStride,
+            )
+        )
         self.model.add(layer=ReLU())
 
-        self.model.add(layer=Conv2D(filters=384, kernel_size=(3, 3)))
+        self.model.add(
+            layer=Conv2D(
+                filters=384,
+                kernel_size=self.common.kernelStride,
+            )
+        )
         self.model.add(layer=ReLU())
 
-        self.model.add(layer=Conv2D(filters=256, kernel_size=(3, 3)))
+        self.model.add(
+            layer=Conv2D(
+                filters=256,
+                kernel_size=self.common.kernelStride,
+            )
+        )
         self.model.add(layer=ReLU())
         self.model.add(layer=MaxPool2D())
 
         self.model.add(layer=Flatten())
 
-        self.model.add(layer=Dense(units=4096))
+        self.model.add(layer=Dense(units=self.common.denseUnits))
         self.model.add(layer=ReLU())
 
-        self.model.add(layer=Dense(units=4096))
+        self.model.add(layer=Dense(units=self.common.denseUnits))
         self.model.add(layer=ReLU())
 
         self.model.add(layer=Dropout(rate=0.5))
 
-        self.model.add(layer=Dense(units=outputUnits, activation="softmax"))
+        self.model.add(
+            layer=Dense(
+                units=self.common.outputFeatueCount,
+                activation="softmax",
+            )
+        )
 
         self.model.compile(
             optimizer="adam",
